7568699ea7b9b1f1e12553f841f8920fdb482c46
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-09 15:38:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]:value});
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  Object[key] = value;
+  object[key] = value;
   return object;
 }
 function deleteFromObjectByKey(object, key){
