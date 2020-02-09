c04a1283433609f7d31d8e91c57b83833fef75ad
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-09 15:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  Object[key] = value
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
